cbc891169aac763ff45acad17f1cfc146c9b18e2
yasin <yasinmahendra26@gmail.com>
2024-12-02 16:53:10 +0700

    fix: fix the button a bit

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:baket_mobile/features/articles/pages/test.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -10,52 +11,55 @@ class App extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    
+
     return MaterialApp(
       title: "BaKet",
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                child: const Text('Beranda'),
-                onPressed: () {},
+      home: Builder(
+        builder: (context) {
+          return Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    child: const Text('Beranda'),
+                    onPressed: () {},
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                  ElevatedButton(
+                    child: const Text('Katalog'),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: const Text('Forum'),
+                    onPressed: () {},
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                  ElevatedButton(
+                    child: const Text('Artikel'),
+                    onPressed: () {Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const ArticleMain()),
+                      );},
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                  ElevatedButton(
+                    child: const Text('Profile'),
+                    onPressed: () {},
+                  ),
+                ],
               ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                ),
-              ElevatedButton(
-                child: const Text('Katalog'),
-                onPressed: () {},
-              ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                ),
-              ElevatedButton(
-                child: const Text('Forum'),
-                onPressed: () {},
-              ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                ),
-              ElevatedButton(
-                child: const Text('Artikel'),
-                onPressed: () {},
-              ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                ),
-              ElevatedButton(
-                child: const Text('Profile'),
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
-
   }
 }
