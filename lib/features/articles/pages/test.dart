d1870ce416ac5a0c0a4ac8b31e3612a61903d6ed
yasin <yasinmahendra26@gmail.com>
2024-12-04 16:39:48 +0700

    feat: hard coded the article homepage

--- lib/features/articles/pages/test.dart
+++ lib/features/articles/pages/test.dart
@@ -53,6 +53,258 @@ class _ArticleMain extends State<ArticleMain> {
                 ),
               ],
             ),
+            const SizedBox(height: 16.0),
+            Center(
+              child: Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "BP Batam Dukung Penuh Pengembangan Investasi Sektor Inovasi Teknologi di Indonesia",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 4.0),
+                    Text("Posted by: Rhuuzi Wiranata"),
+                    SizedBox(height: 8.0),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 4.0),
+                          child: Icon(Icons.thumb_up,),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 16.0),
+                          child: Text("0"),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 4.0),
+                          child: Icon(Icons.comment,),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 0.0),
+                          child: Text("0"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Center(
+              child: Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "King Koil Batam Hadirkan Teknologi Terbaru di Kasur yang Bikin Tidur Makin Lelap",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 4.0),
+                    Text("Posted by: Aminudin"),
+                    SizedBox(height: 8.0),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 4.0),
+                          child: Icon(Icons.thumb_up,),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 16.0),
+                          child: Text("0"),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 4.0),
+                          child: Icon(Icons.comment,),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 0.0),
+                          child: Text("0"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Center(
+              child: Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Viral Pabrik Perusahaan Termahal di Dunia Nvidia Ada di Batam, Ini Faktanya",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 4.0),
+                    Text("Posted by: Kamila Meilina"),
+                    SizedBox(height: 8.0),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 4.0),
+                          child: Icon(Icons.thumb_up,),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 16.0),
+                          child: Text("0"),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 4.0),
+                          child: Icon(Icons.comment,),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 0.0),
+                          child: Text("0"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Center(
+              child: Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Telkom Desain Ulang Pusat Data di Batam supaya Bisa Pakai AI",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 4.0),
+                    Text("Posted by: Amelia Yesidora"),
+                    SizedBox(height: 8.0),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 4.0),
+                          child: Icon(Icons.thumb_up,),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 16.0),
+                          child: Text("0"),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 4.0),
+                          child: Icon(Icons.comment,),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 0.0),
+                          child: Text("0"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Center(
+              child: Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Diskominfo Batam Dorong Generasi Muda Melek Digital melalui Pemanfaatan Teknologi Informasi",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 4.0),
+                    Text("Posted by: Abdul Aziz Maulana"),
+                    SizedBox(height: 8.0),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 4.0),
+                          child: Icon(Icons.thumb_up,),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 16.0),
+                          child: Text("0"),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 4.0),
+                          child: Icon(Icons.comment,),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 0.0),
+                          child: Text("0"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            // const SizedBox(height: 16.0),
+            // Center(
+            //   child: Container(
+            //     padding: const EdgeInsets.all(16.0),
+            //     decoration: BoxDecoration(
+            //       border: Border.all(color: Colors.black),
+            //       borderRadius: BorderRadius.circular(8.0),
+            //     ),
+            //     child: const Column(
+            //       crossAxisAlignment: CrossAxisAlignment.start,
+            //       children: [
+            //         Text(
+            //           "Diskominfo Batam Dorong Generasi Muda Melek Digital melalui Pemanfaatan Teknologi Informasi",
+            //           style: TextStyle(fontWeight: FontWeight.bold),
+            //         ),
+            //         SizedBox(height: 4.0),
+            //         Text("Posted by: Ahmadi Sultan"),
+            //         SizedBox(height: 8.0),
+            //         Row(
+            //           children: [
+            //             Padding(
+            //               padding: EdgeInsets.only(right: 4.0),
+            //               child: Icon(Icons.thumb_up,),
+            //             ),
+            //             Padding(
+            //               padding: EdgeInsets.only(right: 16.0),
+            //               child: Text("0"),
+            //             ),
+            //             Padding(
+            //               padding: EdgeInsets.only(right: 4.0),
+            //               child: Icon(Icons.comment,),
+            //             ),
+            //             Padding(
+            //               padding: EdgeInsets.only(right: 0.0),
+            //               child: Text("0"),
+            //             ),
+            //           ],
+            //         ),
+            //       ],
+            //     ),
+            //   ),
+            // ),
           ],
         ),
       ),
