cd75b6ad6baad1bc87e932f879cc1ce6dc167538
yasin <yasinmahendra26@gmail.com>
2024-12-17 13:24:10 +0700

    build: integrate articlemain.dart with django, next do the search logic

--- lib/features/articles/pages/articlemain.dart
+++ lib/features/articles/pages/articlemain.dart
@@ -1,5 +1,9 @@
+import 'package:baket_mobile/core/constants/_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:baket_mobile/features/articles/pages/articlepage.dart';
+import 'package:baket_mobile/features/articles/models/article.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 
 class ArticleMain extends StatefulWidget {
   const ArticleMain({super.key});
@@ -9,101 +13,138 @@ class ArticleMain extends StatefulWidget {
 }
 
 class _ArticleMain extends State<ArticleMain> {  
-  final List<ArticleCard> items = [
-    const ArticleCard(title: "BP Batam Dukung Penuh Pengembangan Investasi Sektor Inovasi Teknologi di Indonesia", postedBy: "Rhuuzi Wiranata", likeCount: 0, commentCount: 0),
-    const ArticleCard(title: "King Koil Batam Hadirkan Teknologi Terbaru di Kasur yang Bikin Tidur Makin Lelap", postedBy: "Aminudin", likeCount: 0, commentCount: 0),
-    const ArticleCard(title: "Viral Pabrik Perusahaan Termahal di Dunia Nvidia Ada di Batam, Ini Faktanya", postedBy: "Kamila Meilina", likeCount: 0, commentCount: 0),
-    const ArticleCard(title: "Telkom Desain Ulang Pusat Data di Batam supaya Bisa Pakai AI", postedBy: "Amelia Yesidora", likeCount: 0, commentCount: 0),
-    const ArticleCard(title: "Diskominfo Batam Dorong Generasi Muda Melek Digital melalui Pemanfaatan Teknologi Informasi", postedBy: "Abdul Aziz Maulana", likeCount: 0, commentCount: 0),
-    const ArticleCard(title: "Diskominfo Batam Dorong Generasi Muda Melek Digital melalui Pemanfaatan Teknologi Informasi", postedBy: "Abdul Aziz Maulana", likeCount: 0, commentCount: 0),
-    const ArticleCard(title: "Diskominfo Batam Dorong Generasi Muda Melek Digital melalui Pemanfaatan Teknologi Informasi", postedBy: "Abdul Aziz Maulana", likeCount: 0, commentCount: 0),
-    const ArticleCard(title: "Diskominfo Batam Dorong Generasi Muda Melek Digital melalui Pemanfaatan Teknologi Informasi", postedBy: "Abdul Aziz Maulana", likeCount: 0, commentCount: 0),
-    const ArticleCard(title: "Diskominfo Batam Dorong Generasi Muda Melek Digital melalui Pemanfaatan Teknologi Informasi", postedBy: "Abdul Aziz Maulana", likeCount: 0, commentCount: 0),
-  ];
+  static const String baseUrl = Endpoints.baseUrl;
+
+  Future<List<ArticleCard>> fetchArticles(CookieRequest request) async {
+    final response = await request.get('$baseUrl/articles/json/flutter/main/');
+
+    var data = response;
+    Article? article;
+
+    List<ArticleCard> lst = [];
+    for (var d in data) {
+      if (d != null) {
+        article = Article.fromJson(d);
+        lst.add(ArticleCard(
+          key: Key(article.pk), 
+          title: article.fields.title, 
+          postedBy: article.fields.postedBy, 
+          likeCount: article.fields.likeCount, 
+          commentCount: article.fields.commentCount
+        ));
+      }
+    }
+
+    return lst;
+  }
 
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Article'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(height: 16.0),
-            Row(
-              children: [
-                const Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Search Articles',
-                      enabledBorder: OutlineInputBorder(),
-                      border: OutlineInputBorder(),
-                    ),
+      body: FutureBuilder(
+        future: fetchArticles(request),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          else {
+            if (!snapshot.hasData) {
+              return const Column(
+                children: [
+                  Text(
+                    "Tidak ada produk",
+                    style: TextStyle(fontSize: 20, color: Color(0xff59A5D8)),
                   ),
-                ),
-                SizedBox(
-                  height: 48,
-                  child: TextButton(
-                    onPressed: () {},
-                    style: TextButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.horizontal(
-                          right: Radius.circular(20),
-                        ),
-                        side: BorderSide(
-                          width: 1,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                    child: const Text('Search'),
-                  ),
-                ),
-                SizedBox(
-                  height: 48,
-                  child: PopupMenuButton<String>(
-                    onSelected: (value) {
-                      // BELUM HEHE
-                    },
-                    itemBuilder: (context) {
-                      return <String>['Like Terbanyak', 'Terlama', 'Terbaru'].map((e) {
-                        return PopupMenuItem<String>(
-                          value: e,
-                          child: Text(e),
-                        );
-                      }).toList();
-                    },
-                    child: const Row(
+                  SizedBox(height: 8),
+                ],
+              );
+            }
+            else {
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 16.0),
+                    Row(
                       children: [
-                        Text('Sort by'),
-                        SizedBox(width: 8),
-                        Icon(Icons.arrow_drop_down),
+                        const Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              labelText: 'Search Articles',
+                              enabledBorder: OutlineInputBorder(),
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 48,
+                          child: TextButton(
+                            onPressed: () {},
+                            style: TextButton.styleFrom(
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.horizontal(
+                                  right: Radius.circular(20),
+                                ),
+                                side: BorderSide(
+                                  width: 1,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ),
+                            child: const Text('Search'),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 48,
+                          child: PopupMenuButton<String>(
+                            onSelected: (value) {
+                              // BELUM HEHE
+                            },
+                            itemBuilder: (context) {
+                              return <String>['Like Terbanyak', 'Terlama', 'Terbaru'].map((e) {
+                                return PopupMenuItem<String>(
+                                  value: e,
+                                  child: Text(e),
+                                );
+                              }).toList();
+                            },
+                            child: const Row(
+                              children: [
+                                Text('Sort by'),
+                                SizedBox(width: 8),
+                                Icon(Icons.arrow_drop_down),
+                              ],
+                            ),
+                          ),
+                        ),
                       ],
                     ),
-                  ),
+                    const SizedBox(height: 16.0),
+                    Expanded(
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: snapshot.data![index],
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            const SizedBox(height: 16.0),
-            Expanded(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: items.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: items[index],
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
+              );
+            }
+          }
+        }
       ),
     );
   }
@@ -117,7 +158,7 @@ class ArticleCard extends StatelessWidget {
   final int commentCount;
 
   const ArticleCard({
-    super.key, 
+    super.key,
     required this.title,
     required this.postedBy,
     required this.likeCount,
